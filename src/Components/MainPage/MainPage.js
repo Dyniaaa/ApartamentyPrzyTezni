96be1d3390470bdd0b929a93201b8a4aa3d1a888
Dyniaaa <piotrkkardynal@gmail.com>
2024-02-16 19:17:44 +0100

    Delete OA, Location, APT and change background main

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import "./MainPage.scss";
 import Footer from "../Footer/Footer";
 import Main from "./Main/Main";
-import APT from "./APT/APT";
-import OurApartments from "./OurApartments/OurApartments";
-import Location from "./Location/Location";
 
 class MainPage extends Component {
   render() {
     return (
       <section className="mainPage">
         <Main />
-        <APT />
-        <OurApartments />
-        <Location />
         <Footer />
       </section>
     );
